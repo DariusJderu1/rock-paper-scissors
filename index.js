15e6b1fb9cd044028f5e427e38ca917f4c765837
DariusJderu1 <jderudarius@gmail.com>
2025-12-28 18:13:21 +0200

    Add playRound function

--- index.js
+++ index.js
@@ -20,5 +20,75 @@ function getHumanChoice() {
     return prompt("Please enter one of these 3 values: rock, paper, scissors");
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+
+    switch(humanChoice) {
+
+        case "rock":
+
+            if(computerChoice === "rock")
+                console.log("Draw!");
+            
+            else if(computerChoice === "paper") {
+
+                computerScore++;
+                console.log("You lose! Paper beats Rock.");
+            }
+
+            else {
+
+                humanScore++;
+                console.log("You win! Rock beats scissors.");
+            }
+
+            break;
+
+
+        case "paper":
+
+            if(computerChoice === "rock") {
+
+                humanScore++;
+                console.log("You win! Paper beats rock.");
+            }
+            
+            else if(computerChoice === "paper") 
+                console.log("Draw!");
+            
+            else {
+
+                computerScore++;
+                console.log("You lose! Scissors beats Paper.");
+            }
+
+            break;
+
+
+        default:
+
+            if(computerChoice === "rock") {
+
+                computerScore++;
+                console.log("You lose! Rock beats Scissors.");
+            }
+            
+            else if(computerChoice === "paper") {
+
+                humanScore++;
+                console.log("You win! Scissors beats paper.");
+            }
+
+            else 
+                console.log("Draw!");
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
