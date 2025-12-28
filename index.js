8ce911b9429b6b06b1c6a7d57712f4ddd09bf72c
DariusJderu1 <jderudarius@gmail.com>
2025-12-28 17:47:06 +0200

    Add getComputerChoice function

---
+++ index.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    switch(randomNumber) {
+
+        case 0:
+            return "rock";
+
+        case 1:
+            return "paper";
+
+        default:
+            return "scissors";
+    }
+}
+
